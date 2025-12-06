cb42fd3e5f690d94250927ee2739d7a196b1a56c
Nyakundi316 <nyakundibrian316@gmail.com>
2025-12-06 13:51:33 +0300

    fix: correct package.json syntax and update next.config.js for ES module support

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
-
+export default nextConfig;
