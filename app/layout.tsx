a91e7ab086dcc3e28357a4b510939cf1ccc105bf
Bazanty <bazantyloffan@gmail.com>
2026-09-03 20:06:46 +0300

    add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import { SITE } from "./lib/site";
 import { localBusiness, organization, webSite } from "./lib/schema";
 
 const gaId = process.env.NEXT_PUBLIC_GA_ID ?? "G-REPZTGDZ4Z";
+const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID ?? "1724335618797210";
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE.url),
@@ -75,6 +76,32 @@ export default function RootLayout({
 
         <FloatingWhatsAppButton />
         <AnalyticsEvents />
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${metaPixelId}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element -- Meta requires this exact noscript tracking-pixel pattern. */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
 
         {/* Google Analytics */}
         <Script
